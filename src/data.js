e952e31c9cae5911078df2422cb4ac1d92be4230
Bjorn <bcm27@lie.com>
2019-03-11 10:00:44 -0500

    Updated data with better quotes

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'Bjørn Mathisen - portfolio and resume',
   header: "Hi, I'm Bjørn Mathisen",
-  description: "I'm a recently graduated from Principia College with my Bachelors in Computer Science. I have 4 years of programming experience in a variety of student lead SCRUM/Agile projects and am confident in coding in a variety of languages; C#, C, .net, Python, Java, MYSQL/SQL/MongoDB. Other languages that I have been exposed to are LISP, Ruby, OpCode, Assembly, PHP. HTML/CSS, JavaScript, Masm32 and others. If you would like to see any of my personal and school related projects please see below.",
+  description: "I'm a recent graduate from Principia College with my Bachelors in Computer Science. I have 4 years of programming experience in a variety of student lead SCRUM/Agile projects and am confident in coding in a variety of languages; C#, C, .net, Python, Java, MYSQL/SQL/MongoDB. Other languages that I have been exposed to are LISP, Ruby, OpCode, Assembly, PHP. HTML/CSS, JavaScript, Masm32 and others. If you would like to see any of my personal and school related projects please see below.",
   name: 'Bjørn Mathisen',
   footerTitle: 'Software Engineer',
   email: 'thebcm27@gmail.com',
@@ -13,15 +13,15 @@ module.exports = {
     { icon: 'twitter', url: 'https://twitter.com/Bjornthebrave' },
     { icon: 'github-circled', url: 'https://github.com/bcm27' },
     { icon: 'mail-alt' }, // mail alt automatically links to mailto:email
-    { icon: 'dribbble', url: '//' },
-    { icon: 'skype', url: '//' },
+    { icon: 'dribbble', url: 'www.bjornmathisen.com' },
+    { icon: 'skype', url: 'https://join.skype.com/invite/uPjPp0aCAx4h' },
     { icon: 'linkedin-squared', url: 'https://www.linkedin.com/in/bjorn-mathisen-48a69528' },
   ],
   projects: [
     {
       name: 'Customer Management System ',
       description: 'Desktop WPF C# application developed for a client that allows the user to manage bookings, clients, and view a calendar of future gigs, all hosted on a virtual SQL server.',
-      tags: ['C#', 'SQL', 'Database', 'CRM', 'SCRUM', 'Issue Tracking', 'Unit Testing', 'Freelance Work'],
+      tags: ['C#', 'WPF', 'Database', 'CRM', 'SCRUM', 'Issue Tracking', 'Unit Testing', 'Freelance Work'],
       alt: 'Combustion Screenshot', // alt description of image for accessibility. defaults to '{{name}} Screenshot'
       img: 'combustion.png',
       url: 'https://github.com/bcm27/CRM_Basic'
@@ -37,7 +37,7 @@ module.exports = {
       name: "Python Chat Server and Client",
       description: '',
       tags: ['Python', 'Group', 'Undergraduate', 'Server/Client', 'GUI', 'Database'],
-      url: 'htpps://github.com/bcm27/ChatApp'
+      url: 'https://github.com/bcm27/ChatApp'
     }
   ],
   experiences: [
@@ -53,7 +53,6 @@ module.exports = {
     {
       title: 'Observatory Manager at Principia College',
       timeline: 'January 2015 - December 2019',
-      description: 'Implemented Gamification for system',
       responsibilities: [
         'Organized and trained over eight student operators; maintained the observatory schedule and organized public campus-wide events on a weekly basis.'
     ,'Updated training manuals and facilitated the upgrade from DOS to EXP for the telescope control system.'
@@ -69,8 +68,8 @@ module.exports = {
       title: 'CEO at Company'
     },
     {
-      quote: 'Wow, thank you for this, you probably have the most intuitive explanation for this problem!',
-      name: 'Person Name'
+      quote: 'Wow, thank you for this, you were incredibly helpful and your explanation was very clear!',
+      name: 'Martina Carter'
     }
   ]
 }
